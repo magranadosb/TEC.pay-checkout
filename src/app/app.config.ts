73f41a72793272ba577c7e1cc57316588acdb903
magranadosb <t-margranados@itesm.mx>
2024-07-16 12:22:42 -0600

    Se agregan cambios.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
